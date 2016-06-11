ca06bb4d7974576275cee7ef2ea987a63e1e5212
michelle12369 <p5i7gaaaa@gmail.com>
2016-06-11 10:37:48 +0800

    add road report button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
- var clicks = [true, true, true, true];
+ var clicks = [true, true, true, true, true];
     $(".classification").on("click", function() {
         var i = getClickStatus($(this));
+        //console.log(i);
         if (clicks[i] === true) {
             clicks[i] = false;
             $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>"+$(this).text() );
@@ -12,10 +13,37 @@
     });
 
     function getClickStatus(element) {
-        var ids = ["spot", "food", "rest", "repair"];
+        var ids = ["spot", "food", "rest", "repair","Barrier"];
         for (i = 0; i < ids.length; i++) {
             if (ids[i] === element.context.id) {
                 return i;
             }
         }
     }
+/*for footer javaascript*/
+var IsReportDisplay=false;
+$("#down-pic").on("click", function() {
+    if(!IsReportDisplay)
+    {
+        $("#report").css("display","block");
+        IsReportDisplay=true;
+    }
+    else
+    {
+        $("#report").css("display","none");
+        IsReportDisplay=false;
+    }
+})
+var report_clicks = [true, true, true, true, true];
+    $(".report_classification").on("click", function() {
+        var i = getClickStatus($(this));
+        //console.log(i);
+        if (report_clicks[i] === true) {
+            report_clicks[i] = false;
+            $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>"+$(this).text() );
+            
+        } else {
+            report_clicks[i] = true;
+            $(this).html($(this).text());
+        }
+    });
