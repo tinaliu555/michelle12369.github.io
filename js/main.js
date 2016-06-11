f61520608df1b7810f4c50c359f0b48084d79f83
michelle12369 <p5i7gaaaa@gmail.com>
2016-06-11 11:37:42 +0800

    revise .intro

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
  var clicks = [true, true, true, true, true];
-    $(".classification").on("click", function() {
-        var i = getClickStatus($(this));
-        //console.log(i);
-        if (clicks[i] === true) {
-            clicks[i] = false;
-            $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>"+$(this).text() );
-            
-        } else {
-            clicks[i] = true;
-            $(this).html($(this).text());
-        }
-    });
+ $(".classification").on("click", function() {
+     var i = getClickStatus($(this));
+     //console.log(i);
+     if (clicks[i] === true) {
+         clicks[i] = false;
+         $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>" + $(this).text());
 
-    function getClickStatus(element) {
-        var ids = ["spot", "food", "rest", "repair","Barrier"];
-        for (i = 0; i < ids.length; i++) {
-            if (ids[i] === element.context.id) {
-                return i;
-            }
-        }
-    }
-/*for footer javaascript*/
-var IsReportDisplay=false;
-$("#down-pic").on("click", function() {
-    if(!IsReportDisplay)
-    {
-        // $("#report").css("display","block");
-        $("#report").slideToggle("slow");
-        IsReportDisplay=true;
-    }
-    else
-    {
-        $("#report").css("display","none");
-        IsReportDisplay=false;
-    }
-})
-var report_clicks = [true, true, true, true, true];
-    $(".report_classification").on("click", function() {
-        var i = getClickStatus($(this));
-        //console.log(i);
-        if (report_clicks[i] === true) {
-            report_clicks[i] = false;
-            $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>"+$(this).text() );
-            
-        } else {
-            report_clicks[i] = true;
-            $(this).html($(this).text());
-        }
-    });
+     } else {
+         clicks[i] = true;
+         $(this).html($(this).text());
+     }
+ });
+
+ function getClickStatus(element) {
+     var ids = ["spot", "food", "rest", "repair", "Barrier"];
+     for (i = 0; i < ids.length; i++) {
+         if (ids[i] === element.context.id) {
+             return i;
+         }
+     }
+ }
+ /*for footer javaascript*/
+ var IsReportDisplay = false;
+ $("#down-pic").on("click", function() {
+     if (!IsReportDisplay) {
+         $("footer").css("background-color", "transparent");
+         $("#report").slideToggle("slow");
+         IsReportDisplay = true;
+     } else {
+         
+         // $("#report").css("display", "none");
+         $("#report").slideUp("slow",function(){
+            $("footer").css("background-color", "white");
+         });
+         
+         IsReportDisplay = false;
+     }
+ })
+ var report_clicks = [true, true, true, true, true];
+ $(".report_classification").on("click", function() {
+     var i = getClickStatus($(this));
+     //console.log(i);
+     if (report_clicks[i] === true) {
+         report_clicks[i] = false;
+         $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>" + $(this).text());
+
+     } else {
+         report_clicks[i] = true;
+         $(this).html($(this).text());
+     }
+ });
