e6d2c1367e99836626e3665545d307237c88d90e
michelle12369 <p5i7gaaaa@gmail.com>
2016-06-24 00:58:41 +0800

    add alert

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var config = {
 
 var f2 = firebase.initializeApp(config, "Secondary");
 //get current place location
-var path=window.location.pathname.split("/")[1].split(".")[0];
+var path = window.location.pathname.split("/")[1].split(".")[0];
 console.log(path);
 var storageRef = f2.storage().ref();
 var clicks = [true, true, true, true, true];
@@ -81,14 +81,17 @@ function getClickStatus(element) {
 /*for footer javascript*/
 var IsReportDisplay = false;
 $("#down-pic").on("click", function() {
-    if (!IsReportDisplay) {
-        $("footer").css("background-color", "transparent");
-        $("#report").slideToggle("slow");
-        IsReportDisplay = true;
-    } else {
-        $("#report").slideUp("slow");
-        IsReportDisplay = false;
-    }
+    $("#report").slideToggle("slow");
+    // if (!IsReportDisplay) {
+    //     // $("footer").css("background-color", "transparent");
+    //     // $("#report").slideToggle("slow");
+    //     IsReportDisplay = true;
+    //     console.log(IsReportDisplay);
+    // } else {
+    //     // $("#report").slideUp("slow");
+    //     IsReportDisplay = false;
+    //     console.log(IsReportDisplay);
+    // }
 });
 var report_clicks = [false, false, false, false, false];
 $(".report_classification").on("click", function() {
@@ -96,35 +99,35 @@ $(".report_classification").on("click", function() {
     if (report_clicks[i] === true) {
         report_clicks[i] = false;
         $(this).html($(this).text());
+
     } else {
         report_clicks[i] = true;
         $(this).html("<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>" + $(this).text());
-        //console.log($(this).text()+" "+report_clicks);
     }
 });
 var nowPos;
-$("#getPos").click(function() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            nowPos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            console.log(nowPos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-});
+// $("#getPos").click(function() {
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        nowPos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        console.log(nowPos);
+    }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+    });
+} else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+}
+// });
 
 $("#send").on("click", function(e) {
     e.preventDefault();
-    $("footer").css("background-color", "transparent");
+    // $("footer").css("background-color", "transparent");
     $("#report").slideToggle("slow");
-    IsReportDisplay = true;
+    // IsReportDisplay = true;
 
     var select = -1;
     for (var i = 0; i < report_clicks.length; i++) {
@@ -135,35 +138,37 @@ $("#send").on("click", function(e) {
         }
     }
 
+
     var picFile = $("#picData")[0].files[0];
     var input = $("#typeinfo").val();
     //console.log(select+" "+input+" "+picFile);
     if (select == -1 || picFile == undefined) {
-        console.log("Haven't select picture or button!");
+        alert("請選擇類型或是圖片");
         return;
     } else if (input == "") {
-        console.log("Haven't write down the discripition!");
+        alert("請輸入完整資訊!");
         return;
     }
     switch (select) {
         case 0:
-            var data = f2.database().ref(path+"/spot").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
+
+            var data = f2.database().ref(path + "/spot").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
             uploadImg(picFile, "spot", data.key);
             break;
         case 1:
-            var data = f2.database().ref(path+"/food").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
+            var data = f2.database().ref(path + "/food").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
             uploadImg(picFile, "food", data.key);
             break;
         case 2:
-            var data = f2.database().ref(path+"/rest").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
+            var data = f2.database().ref(path + "/rest").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
             uploadImg(picFile, "rest", data.key);
             break;
         case 3:
-            var data = f2.database().ref(path+"/repair").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
+            var data = f2.database().ref(path + "/repair").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
             uploadImg(picFile, "repair", data.key);
             break;
         case 4:
-            var data = f2.database().ref(path+"/barrier").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
+            var data = f2.database().ref(path + "/barrier").push({ "lat": nowPos.lat, "long": nowPos.lng, "word": input });
             uploadImg(picFile, "barrier", data.key);
             break;
         default:
@@ -173,15 +178,17 @@ $("#send").on("click", function(e) {
 });
 
 function uploadImg(picFile, sort, key) {
-    var uploadTask = storageRef.child('images/'+path+'/' + sort + "/" + key + ".png").put(picFile);
+    var uploadTask = storageRef.child('images/' + path + '/' + sort + "/" + key + ".png").put(picFile);
     uploadTask.on('state_changed', function(snapshot) {
         var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log('Upload is ' + progress + '% done');
+
     }, function(error) {
         console.log(error);
     }, function() {
-
-    });
+       $("header").prepend('<div class="alert alert-success alert-dismissible" role="alert" style="margin-left:20px; margin-right:20px;"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>上傳完成</div>');
+        $("header .alert").fadeIn("slow");
+  });
     //恢復預設值
     $("#picData").val("");
     $("#typeinfo").attr("placeholder", "在此輸入簡介").val("").focus().blur();
